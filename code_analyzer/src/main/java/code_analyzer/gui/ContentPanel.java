62589c6240b161385fb472bb6e92dfc9f6ebad87
qwerty367 <Daenisvolkov@gmail.com>
2017-04-08 15:23:28 +0300

    GUI reworked, Added TableSelectionListener example(it listen what table choosed and must to write thats table fields list)

--- code_analyzer/src/main/java/code_analyzer/gui/ContentPanel.java
+++ code_analyzer/src/main/java/code_analyzer/gui/ContentPanel.java
@@ -1,18 +1,37 @@
 package code_analyzer.gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 public class ContentPanel extends JPanel {
+	JScrollPane tableListPanel;
+	JList tableList;
+	JLabel fieldList;
+
 	ContentPanel() {
-		this.setLayout(new GridLayout(1, 1));
+		this.setLayout(new GridLayout(1, 2));
 		this.setPreferredSize(new Dimension(600, 600));
-		JTextField contentField = new JTextField();
-		this.add(contentField);
-		// contentField.setPreferredSize(new Dimension(100, 30));
+
+		String TableListSrting[] = { "Tanble1", "Tanble2", "Tanble3" };
+		tableList = new JList(TableListSrting);
+		tableListPanel = new JScrollPane(tableList);
+		TableListSelectionListener tableListSelectionListener = new TableListSelectionListener(this);
+		tableList.addListSelectionListener(tableListSelectionListener);
+		this.add(tableListPanel);
+
+		fieldList = new JLabel();
+		fieldList.setVerticalAlignment(SwingConstants.TOP);
+		fieldList.setForeground(Color.GREEN);
+		this.setBackground(Color.BLACK);
+		this.add(fieldList);
+
 	}
 
 }
---
+++ code_analyzer/src/main/java/code_analyzer/gui/TableListSelectionListener.java
@@ -0,0 +1,21 @@
+package code_analyzer.gui;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class TableListSelectionListener implements ListSelectionListener {
+	ContentPanel contentPanel;
+
+	TableListSelectionListener(ContentPanel contentPanel) {
+		this.contentPanel = contentPanel;
+	}
+
+	public void valueChanged(ListSelectionEvent e) {
+		{
+			String s = (String) contentPanel.tableList.getSelectedValue();
+			contentPanel.fieldList.setText(s + " Field list:");
+
+		}
+
+	}
+}
