35e291757643af314a8cfe0d6eae2951379f565e
elldissin <elldissin@gmail.com>
2017-05-01 10:57:13 +0300

    added tests for other arguments

--- code_analyzer/src/test/java/code_analyzer/ArgumentFinderTest.java
+++ code_analyzer/src/test/java/code_analyzer/ArgumentFinderTest.java
@@ -16,12 +16,30 @@ public class ArgumentFinderTest {
 		List<ExpressionArgument> argumentListTest = new ArrayList<ExpressionArgument>();
 		argumentListTest = argumentFinderTest.getArgumentListFor("Something(\"aa\",0,\"dd\")");
 		String firstArgumentName, firstArgumentValue, firstArgumentType;
+		String secondArgumentName, secondArgumentValue, secondArgumentType;
+		String thirdArgumentName, thirdArgumentValue, thirdArgumentType;
 		firstArgumentName = argumentListTest.get(0).getName();
 		firstArgumentValue = argumentListTest.get(0).getValue();
 		firstArgumentType = argumentListTest.get(0).getType();
-		assertTrue("Wrong argument name:" + firstArgumentName, firstArgumentName.equals("StubName"));
-		assertTrue("Wrong argument type:" + firstArgumentType, firstArgumentType.equals("StubType"));
-		assertTrue("Wrong argument value:" + firstArgumentValue, firstArgumentValue.equals("\"aa\""));
+
+		secondArgumentName = argumentListTest.get(1).getName();
+		secondArgumentValue = argumentListTest.get(1).getValue();
+		secondArgumentType = argumentListTest.get(1).getType();
+
+		thirdArgumentName = argumentListTest.get(2).getName();
+		thirdArgumentValue = argumentListTest.get(2).getValue();
+		thirdArgumentType = argumentListTest.get(2).getType();
+		assertTrue("Wrong argument 1 name:" + firstArgumentName, firstArgumentName.equals("StubName"));
+		assertTrue("Wrong argument 1 type:" + firstArgumentType, firstArgumentType.equals("StubType"));
+		assertTrue("Wrong argument 1 value:" + firstArgumentValue, firstArgumentValue.equals("\"aa\""));
+
+		assertTrue("Wrong argument 2 name:" + secondArgumentName, secondArgumentName.equals("StubName"));
+		assertTrue("Wrong argument 2 type:" + secondArgumentType, secondArgumentType.equals("StubType"));
+		assertTrue("Wrong argument 2 value:" + secondArgumentValue, secondArgumentValue.equals("0"));
+
+		assertTrue("Wrong argument 3 name:" + thirdArgumentName, thirdArgumentName.equals("StubName"));
+		assertTrue("Wrong argument 3 type:" + thirdArgumentType, thirdArgumentType.equals("StubType"));
+		assertTrue("Wrong argument 3 value:" + thirdArgumentValue, thirdArgumentValue.equals("\"dd\""));
 	}
 
 }
